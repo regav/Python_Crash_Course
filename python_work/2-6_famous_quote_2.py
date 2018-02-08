b1690f9cae18ca80f038371c02ecd533fa9b07bd
regav <reg@rincon.com>
2018-02-08 15:28:13 -0700

    add comments to previous programs for practice 2-10, pg 34

--- python_work/2-6_famous_quote_2.py
+++ python_work/2-6_famous_quote_2.py
@@ -1,3 +1,5 @@
+# This program has 3 varaibles to make one simple print line.
+# the "\"" was used to add quotes within the string so it would look correct
 famous_person = "John Lennon"
 famous_person_quote = "Time you enjoy wasting, was not wasted."
 message = famous_person + " once said, " + "\"" + famous_person_quote + "\""
--- python_work/2-8_number_eight.py
+++ python_work/2-8_number_eight.py
@@ -1,3 +1,5 @@
+#one can print evaluations of equations directly within the print()
+# rather than creating a varaible of the equation you want
 print(5 + 3)
 print(16 / 2)
 print(2 * 4)
