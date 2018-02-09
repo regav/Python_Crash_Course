7cd9f110a0a4eb6cfbb0c41e45ee7a595c8d97ef
regav <reg@rincon.com>
2018-02-09 16:59:20 -0700

    show how to use .insert() method and use of del statement to remove an entry

--- python_work/3_lists_chapter/motorcycles.py
+++ python_work/3_lists_chapter/motorcycles.py
@@ -4,3 +4,7 @@ motorcycles[0] = 'ducati'
 print(motorcycles)
 motorcycles.append('kawasaki')
 print(motorcycles)
+motorcycles.insert(1, 'ninja')
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
