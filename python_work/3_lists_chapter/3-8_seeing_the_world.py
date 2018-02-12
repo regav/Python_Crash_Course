ee308660cccc457f202389d3fbbb7d8488d0a7b4
regav <reg@rincon.com>
2018-02-12 15:01:50 -0700

    TRY IT YOURSELF 3-8 pg 50, practice using lists and sort functions

---
+++ python_work/3_lists_chapter/3-8_seeing_the_world.py
@@ -0,0 +1,22 @@
+# think of 5 places you want to go and put them into a list
+# make sure to not put them in alpha order
+
+locations = ['greece', 'colorado', 'japan', 'south korea', 'africa']
+print("This is my list of places I wish to see.")
+print(locations)
+print("This is my list sorted.")
+print(sorted(locations))
+print("This is my original list")
+print(locations)
+locations.reverse()
+print("This is my list in reverse order.")
+print(locations)
+locations.reverse()
+print("This is my list again back in order.")
+print(locations)
+locations.sort()
+print("This is my list sorted.")
+print(locations)
+locations.sort(reverse=True)
+print("This is my list in reverse order.")
+print(locations)
