5ed1508d7e786afa140a078d35da39d39cfd18a0
regav <reg@rincon.com>
2018-02-12 15:25:18 -0700

    add 3-9 to folder, add print of len(guests) to line 12. show use of len() on a list

---
+++ python_work/3_lists_chapter/3-9_dinner_guests.py
@@ -0,0 +1,12 @@
+guests = ['Albert Wesker', 'Chris Redfield', 'Lois lane']
+invitation = ', will you attend my dinner party this weekend?'
+cant_arrive = ' cannot attend the party.'
+print(guests[0] + invitation)
+print(guests[1] + invitation)
+print(guests[2] + invitation)
+print(guests[0] + cant_arrive)
+guests[0] = 'John Lennon'
+print(guests[0] + invitation)
+print(guests[1] + invitation)
+print(guests[2] + invitation)
+print("There will be " + str(len(guests)) + " people attending the party.")
