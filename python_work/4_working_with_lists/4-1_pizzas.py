a2328b847a0266b8378bfaba2142d9c67509f0a6
regav <reg@rincon.com>
2018-02-21 16:13:04 -0700

    TRY IT YOURSELF 4-1_pizzas.py pg 60

---
+++ python_work/4_working_with_lists/4-1_pizzas.py
@@ -0,0 +1,12 @@
+pizzas = ['mushroom pizza','no-cheese pizza','pepperoni pizza']
+
+for pizza in pizzas:
+   print ("I like " + pizza + ".")
+
+print("I really like pizza.\nI really like pizza, such as:")
+for pizza in pizzas:
+   print("pizza")
+print("I really love pizza!")
+# to follow instructions in book I am manually printing the for loop
+
+print("I really like pizza, such as:\nmushroom pizza\nno-cheese pizza\nand pepperoni pizza\nI really love pizza!")
