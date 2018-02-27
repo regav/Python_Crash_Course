5987a7728496e29a3ef983e4bd0bd6143f60c66f
regav <reg@rincon.com>
2018-02-27 16:28:10 -0700

    Show use of printing wihtout specifying second index number list[x:]

--- python_work/4_working_with_lists/players.py
+++ python_work/4_working_with_lists/players.py
@@ -1,2 +1,10 @@
+
 players = ['charles','martina','michael','florence','eli']
+print("we will print the whole list of players:" + str(players))
 print(players[0:3])
+print("We are going to print the last 3 players in the list.")
+print(players[-3:])
+print("we are going to print index '-1' then '-2' then '-3'.")
+print(players[-1])
+print(players[-2])
+print(players[-3])
