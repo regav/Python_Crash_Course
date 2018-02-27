0d1796b087f1fac4eafa15efb838af3f5f991c85
regav <reg@rincon.com>
2018-02-27 16:50:20 -0700

    Book has us demostrates that both lists are in fact seperate and can be appended to

--- python_work/4_working_with_lists/foods.py
+++ python_work/4_working_with_lists/foods.py
@@ -6,3 +6,11 @@ print(my_foods)
 
 print("\nMy friend's favorite foods are:")
 print(friends_foods)
+
+my_foods.append('cannoli')
+friends_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friends_foods)
