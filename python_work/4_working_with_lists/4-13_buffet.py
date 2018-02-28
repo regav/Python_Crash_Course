1e1498e28b7eb46e11a9061794078ce1add47b87
regav <reg@rincon.com>
2018-02-28 10:49:27 -0700

    TRY IT YOURSELF pg 71, show use of tuples, how to get TYPE ERROR when trying to change a tuple, and using for loops with a Tuple

---
+++ python_work/4_working_with_lists/4-13_buffet.py
@@ -0,0 +1,5 @@
+dishes = ('chicken', 'pizza', 'salad', 'rice', 'beans')
+# dishes[0] = 'banana' # code to demostrate Tuples cant be modifited
+dishes = ('sushi', 'orange chicken', 'pizza', 'salad', 'rice')
+for dish in dishes:
+   print(dish.title())
