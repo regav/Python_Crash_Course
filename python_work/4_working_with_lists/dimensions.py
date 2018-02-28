9fd3cfb79e69b582777f085498d11ae4f9bbefc1
regav <reg@rincon.com>
2018-02-28 10:40:49 -0700

    show that Tuples can be looped through just like a list

--- python_work/4_working_with_lists/dimensions.py
+++ python_work/4_working_with_lists/dimensions.py
@@ -2,4 +2,7 @@ dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
 # we will try to change index 0 value which will give us an error
-dimensions[0] = 250
+# dimensions[0] = 250
+# this code below shows Tuples can be looped through just like a list
+for dimension in dimensions:
+   print(dimension)
