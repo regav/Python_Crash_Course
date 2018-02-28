46765f1a4c099c85e206f6d627a97732c8b5daae
regav <reg@rincon.com>
2018-02-27 17:22:01 -0700

    DO IT YOURSELF lesson 4-10 printing slices of a list

---
+++ python_work/4_working_with_lists/4-10_slices.py
@@ -0,0 +1,17 @@
+# The code will be borrowed from players.py to do this lesson
+players = ['charles','martina','michael','florence','eli']
+print("we will print the whole list of players:" + str(players))
+print(players[0:3])
+print("We are going to print the last 3 players in the list.")
+print(players[-3:])
+print("we are going to print index '-1' then '-2' then '-3'.")
+print(players[-1])
+print(players[-2])
+print(players[-3])
+# The actual lesson code is below
+print("The first three items in the list are:")
+print(players[0:3])
+print("Three items from the middle of the list are:")
+print(players[1:4])
+print("The last three items in the list are:")
+print(players[-3:])
