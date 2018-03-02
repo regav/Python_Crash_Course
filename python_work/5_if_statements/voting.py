5d30f3f31145422554135dbaa592225c5d5744d1
regav <reg@rincon.com>
2018-03-02 16:29:47 -0700

    showing the if-else statement block, else statement indented code only gets executed when the if statement evaulates to False

--- python_work/5_if_statements/voting.py
+++ python_work/5_if_statements/voting.py
@@ -1,4 +1,7 @@
-age = 19
+age = 17
 if age >= 18:
     print("You are old enough to vote.")
     print("Have you registered to vote?")
+else:
+    print("You are not old enough to vote.")
+    print("Please register to vote as soon as you turn 18!")
