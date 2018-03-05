f8db6a489fc311e292b6eb947b30c6385ec25d80
regav <reg@rincon.com>
2018-03-05 11:16:53 -0700

    changed 5 points to a message to reduce print messages

--- python_work/5_if_statements/5-3_alien_colors1.py
+++ python_work/5_if_statements/5-3_alien_colors1.py
@@ -1,9 +1,8 @@
 alien_color = 'green'
-
+point_message = "You have earned 5 points!"
 if alien_color == 'green':
-    print("You have earned 5 points!")
-
+    print(point_message)
 alien_color = 'yellow'
 
 if alien_color == 'green':
-    print("You have earned 5 points!")
+    print(point_message)
