96e4ceeb132014f4acf9b9a87115de42fda1bb59
regav <reg@rincon.com>
2018-03-05 11:24:02 -0700

    TRY IT YOURSELF 5-4 tesing more conditionals with elif and else blocks.

---
+++ python_work/5_if_statements/5-4_alien_colors2.py
@@ -0,0 +1,13 @@
+alien_color = 'yellow'
+green_message = "You earned 5 points!"
+not_green_message = "You earned 10 points!"
+
+if alien_color == 'green':
+   print(green_message) 
+elif alien_color != 'green':
+    print(not_green_message)
+
+if alien_color == 'green':
+    print(green_message)
+else:
+    print(not_green_message)
