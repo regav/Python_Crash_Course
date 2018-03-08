f85bae894d3a35e6dbcd7c6d3c84d9c4ded6f587
regav <reg@rincon.com>
2018-03-07 17:09:01 -0700

    how to test for an empty list. by enshourding the list with an initial if statement

--- python_work/5_if_statements/toppings.py
+++ python_work/5_if_statements/toppings.py
@@ -19,3 +19,14 @@ for requested_topping in requested_toppings:
         print("Adding " + requested_topping + ".")
 
 print("\nFinished making your pizza!")
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping == 'green peppers':
+            print("Sorry we are out of green peppers right now.")
+        else:
+            print("Adding " + requested_topping + ".")
+else:
+    print("\n\nAre you sure you want a plain pizza?")
