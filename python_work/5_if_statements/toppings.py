ebf580d9220544b3cf26381630456cfa718f18c2
regav <reg@rincon.com>
2018-03-08 12:06:12 -0700

    Show how to use multiple list against for loops and with if statements

--- python_work/5_if_statements/toppings.py
+++ python_work/5_if_statements/toppings.py
@@ -30,3 +30,20 @@ if requested_toppings:
             print("Adding " + requested_topping + ".")
 else:
     print("\n\nAre you sure you want a plain pizza?")
+
+# This is the newest version of this program that has multiple lists pg 92
+# I also added the intial if statement to check if requested_toppings is empty
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                      'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping in available_toppings:
+            print("Adding " + requested_topping + ".")
+        else:
+            print("Sorry, we do not have " + requested_topping + ".")
+else:
+    print("Are you sure you want a plain pizza?")
+
+print("\nFinished making your pizza!")
