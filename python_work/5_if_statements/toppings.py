a1321fa9ab30676bcfffb4d8adc120dd0b738147
regav <reg@rincon.com>
2018-03-07 17:03:51 -0700

    more efficent code version of toppings.py with a for loop and also checking for conditonal within the for loop

--- python_work/5_if_statements/toppings.py
+++ python_work/5_if_statements/toppings.py
@@ -1,10 +1,21 @@
-requested_toppings = ['mushrooms', 'extra cheese']
+#requested_toppings = ['mushrooms', 'extra cheese']
+#
+#if 'mushrooms' in requested_toppings:
+#    print("Adding mushrooms.")
+#if 'pepperoni' in requested_toppings:
+#    print("Adding pepperoni.")
+#if 'extra cheese' in requested_toppings:
+#    print("Adding extra cheese.")
+#
+#print("\nFinished making your pizza!")
+# doing a new version of this code PG 90
 
-if 'mushrooms' in requested_toppings:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
+requested_toppings = ['mushrooms', 'extra cheese', 'green peppers']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry we are out of green peppers right now.")
+    else:
+        print("Adding " + requested_topping + ".")
 
 print("\nFinished making your pizza!")
