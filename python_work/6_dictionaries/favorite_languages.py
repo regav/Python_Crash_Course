4112d58641c14ff367881602b78884705ad2bf9b
regav <reg@rincon.com>
2018-03-20 16:08:20 -0700

    show how to loop only through keys of a dictionary by use of .keys() method or by the default behavior of a for loop against a dictionary, also show conditional within a loop of a dictionary to handle exceptions

--- python_work/6_dictionaries/favorite_languages.py
+++ python_work/6_dictionaries/favorite_languages.py
@@ -12,3 +12,13 @@ print("\n\n")
 for name, language in favorite_languages.items():
     print(name.title() + "'s favorite lanugage is " +
     language.title() + ".")
+print("\n\n")
+friends = ['sarah', 'phil']
+
+for name in favorite_languages.keys(): 
+    print(name.title())
+
+    if name in friends:
+        print(" Hi " + name.title() +
+        ", I see your favorite language is " +
+        favorite_languages[name].title() + "!")
