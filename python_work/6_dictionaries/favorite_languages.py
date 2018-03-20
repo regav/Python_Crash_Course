1314982893abd6c836fb3058d0fe23afe8ac2f5e
regav <reg@rincon.com>
2018-03-20 15:14:04 -0700

    show how to loop through a dictionray by using the .items() method and giving two varaibles for the 'for' statement for x,y in dictionary.items():

--- python_work/6_dictionaries/favorite_languages.py
+++ python_work/6_dictionaries/favorite_languages.py
@@ -8,3 +8,7 @@ favorite_languages = {
 print("Sarah's favorite language is " +
     favorite_languages['sarah'].title() +
     ".")
+print("\n\n")
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite lanugage is " +
+    language.title() + ".")
