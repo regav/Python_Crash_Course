0df20bc2f5d788824a1cd0b66854ee5496a65d3e
regav <reg@rincon.com>
2018-03-19 17:26:27 -0700

    add 6-3_glossary.py a simple dictonary to model a real dictonary of defining terms/words used within Python

---
+++ python_work/6_dictionaries/6-3_glossary.py
@@ -0,0 +1,13 @@
+glossary = {
+    'print': "This is the 'print' keyword within Python, it is used to print out a string value to the screen or output onto the screen.",
+    'list': "This is the 'list' varaible within Python. It can be used to store a list of values that can be called out individually or all together or one by one and even a certain group or other desired methods.",
+    'loop': "This is the 'loop' action within Python. It is called by using the keyword 'for' it allows one to iterate over a list to do a certain action or many.",
+    'for': "This is the 'for' keyword within Python to begin a loop statment. This can be used against a list dicitonary or other functions/methods built in Python.",
+    '.title()': "This is the '.title()' method within Python. It is used to apply a certain format against a string, it will captialize each word within a string."
+}
+
+print("print: " + glossary['print'] + "\n")
+print("list: " + glossary['list'] + "\n")
+print("loop: " + glossary['loop'] + "\n")
+print("for: " + glossary['for'] + "\n")
+print(".title(): " + glossary['.title()'] + "\n")
